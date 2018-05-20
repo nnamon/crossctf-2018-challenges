d2a18eff7ddbac39e7a2d38b6471df77c0322b20
amon <amon@nandynarwhals.org>
2018-05-20 12:22:34 -0700

    Updated gruffybear a little.

--- challenges/gruffybear/src/gruffybear.c
+++ challenges/gruffybear/src/gruffybear.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int admin_enabled = 0;
+
+void print_banner() {
+    puts("Forest Fires are Bad!");
+    puts("Stop Smoking, We Love You.");
+    puts("1. Build a Bear");
+    puts("2. Select Favourite Bear");
+    puts("3. Delete Bear");
+
+
+    if (admin_enable) {
+        puts("4. Initialize ");
+    }
+
+    puts("0. Exit");
+}
 
 int main() {
     setvbuf(stdin, NULL, _IONBF, 0);
@@ -13,5 +29,11 @@ int main() {
         print_banner();
 
         scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                puts("Case 1");
+            default:
+                puts("Nothing to do.");
+        }
     }
 }
