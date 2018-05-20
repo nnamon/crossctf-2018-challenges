cb187ea322e799825e51463cac65b49d4e674aa0
amon <amon@nandynarwhals.org>
2018-05-20 12:33:34 -0700

    Moved the challenges to quals

---
+++ quals/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for directory in challenges/*/service/Dockerfile; do
+    echo Building $directory
+    cd $(dirname $directory)
+    chmod +x dockerbuild.sh
+    ./dockerbuild.sh
+    cd $cur_dir
+done
