e2274549f7ef13020fb6229129e64f1c7f4a8aa7
Quan Yang <quanyang@live.com>
2018-06-15 01:17:53 -0700

    Typo Fix

--- finals/challenges/CacheCreek/service/src/login.php
+++ finals/challenges/CacheCreek/service/src/login.php
@@ -173,7 +173,7 @@ if (isset($_GET['register']) && isset($_POST['username'])) {
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">What is CrossCTF?</h5>
-          <p class="grey-text text-lighten-4">CrossCTF is a cyber security competition organized by NTU with collaboration with NUS Greyhats and SMU Whitehats. This also provides a platform for participants to hack safely in Singapore. The other reason is to promote Cyber Security Capture-The-Flag Compeitions in Singapore.</p>
+          <p class="grey-text text-lighten-4">CrossCTF is a cyber security competition organized by NTU with collaboration with NUS Greyhats and SMU Whitehats. This also provides a platform for participants to hack safely in Singapore. The other reason is to promote Cyber Security Capture-The-Flag Competitions in Singapore.</p>
         </div>
       </div>
     </div>
